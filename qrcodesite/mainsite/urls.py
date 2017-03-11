1de6cccd88c488d153514e8f5b4516fb932244b8
倪嘉铭 <oscarnjm@gmail.com>
2017-03-11 17:32:29 -0500

    init

---
+++ qrcodesite/mainsite/urls.py
@@ -0,0 +1,11 @@
+from . import views 
+
+from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
+
+app_name = 'mainsite'
+
+
+urlpatterns = [
+	url(r'^index$', views.index, name='index'),
+]
